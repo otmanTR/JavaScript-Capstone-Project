11ce4183ea0670eda19015b65721fa3c84cbc4ac
ambrose-kibet <ambrosekibet36@gmail.com>
2023-02-14 20:20:45 +0300

    add pop up pokemon

--- src/modules/popUpFunction.js
+++ src/modules/popUpFunction.js
@@ -1,44 +1,45 @@
 import getItems from './getItems';
 
-const nameUrl = 'https://pokeapi.co/api/v2/pokemon/2';
-
-
-const getName =  () => {
- getItems(nameUrl)
-   .then((data) => console.log('we are here', data));
-    
-};
-getName();
 const popUp = document.querySelector('.popUp');
 popUp.innerHTML = '';
 const popUpXButton = document.querySelector('.popUpXButton');
 const home = document.querySelector('.home');
-const popUpItems = `<div class="popUpImage">
-<img
-  src=""
-  name=""
-  alt=""
-/>
-</div>
-<div class="popUpTitle">
-<h2>ulbasaur</h2>
-</div>
-<div class="popUpProperties">
-<ul>
-  <li class="type">Type : Plant</li>
-  <li class="weight">Weight:100</li>
-  <li class="height">${data.height}</li>
-  <li class="power">Power:100021</li>
-</ul>
-</div>`;
 
-popUp.innerHTML += popUpItems;
 popUp.classList.add('close');
 popUpXButton.classList.add('close');
 
 const popUpFunction = (commentButton) => {
   commentButton.forEach((element) => {
     element.addEventListener('click', (e) => {
+      const pokemonId = e.target.id;
+      const getName = () => {
+        getItems(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+          (data) => {
+            const popUpItems = `<div class="popUpImage">
+<img
+  src="${data.sprites.other['official-artwork'].front_default}"
+  name="${data.name}"
+  alt="${data.name}"
+/>
+</div>
+<div class="popUpTitle">
+<h2>${data.name}</h2>
+</div>
+<div class="popUpProperties">
+<ul>
+  <li class="type">Type : ${data.types[0].type.name}</li>
+  <li class="weight">Weight:${data.weight}</li>
+  <li class="height">Heigth:${data.height}</li>
+  <li class="power">baseexperience
+:${data.base_experience}</li>
+</ul>
+</div>`;
+
+            popUp.innerHTML += popUpItems;
+          },
+        );
+      };
+      getName();
       home.classList.add('close');
       popUp.classList.remove('close');
       popUpXButton.classList.remove('close');

9ad1c86e07e8295a9b77311df830abb09b93334e
otmanTR <r.brk.otman@gmail.com>
2023-02-14 16:25:12 +0300

    updated js files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import PopUp from './modules/popUpFunction';
 
 require('./index.css');
-// const takeID = require('./modules/takingId');
-
-// const body = document.querySelector('.body');
 
 PopUp = new PopUp();
 PopUp();
